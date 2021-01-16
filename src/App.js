1d8929d289a85b93c0b45421dc6f268f7898a371
MDC <markdcross@gmail.com>
2021-01-16 09:25:02 -0500

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import NavBar from './components/layout/NavBar';
+// import Home from './components/pages/Home';
+// import Portfolio from './components/pages/Portfolio';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Container>
+        {/* <NavBar /> */}
+        {/* <Switch> */}
+        {/* <Route exact path='/'> */}
+        {/* <Home /> */}
+        {/* </Route> */}
+        {/* <Route exact path='/portfolio' component={Portfolio} /> */}
+        {/* </Switch> */}
+        <Footer />
+      </Container>
+    </Router>
   );
 }
 
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Row from 'react-bootstrap/Row';
+
+const Footer = () => {
+  return (
+    <Row className='credits'>
+      <p className='text-center text-muted'>
+        Created by Mark Cross | Powered by React&nbsp;
+        <i className='fab fa-react'></i> | Copyright © 2020
+      </p>
+      <span className='text-center text-muted'>
+        Photo by
+        <a href='https://unsplash.com/@kovacsz1?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText'>
+          Z S
+        </a>
+        &nbsp;on&nbsp;
+        <a href='https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText'>
+          Unsplash
+        </a>
+      </span>
+    </Row>
+  );
+};
+export default Footer;
