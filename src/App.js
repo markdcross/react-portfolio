1b78c7879233125b53af8191d32c9515396181b5
MDC <markdcross@gmail.com>
2021-01-16 09:45:26 -0500

    Added basic home page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container';
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // import NavBar from './components/layout/NavBar';
-// import Home from './components/pages/Home';
+import Home from './components/pages/Home';
 import Portfolio from './components/pages/Portfolio';
 import Footer from './components/layout/Footer';
 
@@ -12,12 +12,10 @@ function App() {
     <Router>
       <Container>
         {/* <NavBar /> */}
-        {/* <Switch> */}
-        {/* <Route exact path='/'> */}
-        {/* <Home /> */}
-        {/* </Route> */}
-        <Route exact path='/portfolio' component={Portfolio} />
-        {/* </Switch> */}
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/portfolio' component={Portfolio} />
+        </Switch>
         <Footer />
       </Container>
     </Router>
---
+++ src/components/pages/Home.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react';
+// import Intro from '../user/Intro';
+// import Bio from '../user/Bio';
+// import Stack from '../user/Stack';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
+const Home = () => {
+  return (
+    <Fragment>
+      <Jumbotron className='heroDiv'>
+        {/* <Intro /> */}
+        {/* <Bio /> */}
+        {/* <Stack /> */}
+      </Jumbotron>
+    </Fragment>
+  );
+};
+
+export default Home;
