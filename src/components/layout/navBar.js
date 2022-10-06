4a6e6fb5b34ad51e33159dc54db0dfa4f532d4bd
MDC <markdcross@gmail.com>
2022-10-05 21:22:57 -0400

    Add AWS cert to nav bar

--- src/components/layout/navBar.js
+++ src/components/layout/navBar.js
@@ -1,76 +1,87 @@
-import React, {useState} from 'react';
-import Logo from './logo';
-import ContactModal from '../contact/contactModal';
-import Hamburger from './hamburger';
+import React, { useState } from "react";
+import Logo from "./logo";
+import ContactModal from "../contact/contactModal";
+import Hamburger from "./hamburger";
 
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import {LinkContainer} from 'react-router-bootstrap';
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { LinkContainer } from "react-router-bootstrap";
 
 export const NavBar = () => {
-    const [mobileNavOpened, setMobileNavOpened] = useState(false);
-    const toggleMobileNav = () => setMobileNavOpened(!mobileNavOpened);
+  const [mobileNavOpened, setMobileNavOpened] = useState(false);
+  const toggleMobileNav = () => setMobileNavOpened(!mobileNavOpened);
 
-    //* Dismiss navbar on link click
+  //* Dismiss navbar on link click
 
-    return (
-        <Navbar className={mobileNavOpened ? 'nav dropdown-opened' : 'nav'}>
-            <Navbar.Brand className='logo' href='/'>
-                <Logo logo='../img/tinified/logo2.jpg' logoAlt="Mark's logo"/>
-            </Navbar.Brand>
+  return (
+    <Navbar className={mobileNavOpened ? "nav dropdown-opened" : "nav"}>
+      <Navbar.Brand className="logo" href="/">
+        <Logo logo="../img/tinified/logo2.jpg" logoAlt="Mark's logo" />
+      </Navbar.Brand>
 
-            <Hamburger toggleMobileNav={toggleMobileNav} opened={mobileNavOpened}/>
+      <Hamburger toggleMobileNav={toggleMobileNav} opened={mobileNavOpened} />
 
-            <div className='dropdown-link-container'>
-                <div className='dropdown-links'>
-                    <LinkContainer to='/'>
-                        <Nav.Link className='links' onClick={toggleMobileNav}>
-                            Home
-                        </Nav.Link>
-                    </LinkContainer>
+      <div className="dropdown-link-container">
+        <div className="dropdown-links">
+          <LinkContainer to="/">
+            <Nav.Link className="links" onClick={toggleMobileNav}>
+              Home
+            </Nav.Link>
+          </LinkContainer>
 
-                    <LinkContainer to='/portfolio'>
-                        <Nav.Link className='links' onClick={toggleMobileNav}>
-                            Portfolio
-                        </Nav.Link>
-                    </LinkContainer>
+          <LinkContainer to="/portfolio">
+            <Nav.Link className="links" onClick={toggleMobileNav}>
+              Portfolio
+            </Nav.Link>
+          </LinkContainer>
 
-                    <Nav.Link
-                        className='links'
-                        href='https://drive.google.com/file/d/1Rxnp-pGEktrIgEwlOi012DxAKfzH4Yqj/view?usp=sharing'
-                        target='_blank'
-                        rel='noreferrer noopener'
-                    >
-                        Resume
-                    </Nav.Link>
+          <Nav.Link
+            className="links"
+            href="https://drive.google.com/file/d/1Rxnp-pGEktrIgEwlOi012DxAKfzH4Yqj/view?usp=sharing"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Resume
+          </Nav.Link>
 
-                    <ContactModal/>
+          <ContactModal />
 
-                    <Nav.Link
-                        href='https://github.com/markdcross'
-                        target='_blank'
-                        className='links'
-                        rel='noreferrer noopener'
-                    >
-            <span className='icon'>
-              <i className='fab fa-github'></i>
+          <Nav.Link
+            href="https://github.com/markdcross"
+            target="_blank"
+            className="links"
+            rel="noreferrer noopener"
+          >
+            <span className="icon">
+              <i className="fab fa-github"></i>
             </span>
-                    </Nav.Link>
+          </Nav.Link>
 
-                    <Nav.Link
-                        href='https://www.linkedin.com/in/markdcross/'
-                        target='_blank'
-                        className='links'
-                        rel='noreferrer noopener'
-                    >
-            <span className='icon'>
-              <i className='fab fa-linkedin'></i>
+          <Nav.Link
+            href="https://www.linkedin.com/in/markdcross/"
+            target="_blank"
+            className="links"
+            rel="noreferrer noopener"
+          >
+            <span className="icon">
+              <i className="fab fa-linkedin"></i>
             </span>
-                    </Nav.Link>
-                </div>
-            </div>
-        </Navbar>
-    );
+          </Nav.Link>
+
+          <Nav.Link
+            href="https://drive.google.com/file/d/1XUJMV61ixhSc1xFDmMUDRztTKoaCWUCH/view?usp=sharing"
+            target="_blank"
+            className="links"
+            rel="noreferrer noopener"
+          >
+            <span className="icon">
+              <i className="fab fa-aws"></i>
+            </span>
+          </Nav.Link>
+        </div>
+      </div>
+    </Navbar>
+  );
 };
 
 export default NavBar;
